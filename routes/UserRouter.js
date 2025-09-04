1431d8e7f320bfad5552e54e9fcaec25b4a2da78
belliledriss173-crypto <belliledriss173@gmail.com>
2025-09-04 15:34:50 +0100

    commantaire addImages

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -8,7 +8,7 @@ router.get('/getUserById/:id',UserController.getUserById);
 router.get('/getUserByAge/:age',UserController.getUserByAge);
 router.post('/addClient',UserController.addClient);
 router.delete('/deleteUserbyId/:id',UserController.deleteUserbyId);
-router.get('/addClientWithImage',uploadFile.single("user_Image"),UserController.addClientWithImage);
+//router.get('/addClientWithImage',uploadFile.single("user_Image"),UserController.addClientWithImage);
 
 module.exports =router;
 
